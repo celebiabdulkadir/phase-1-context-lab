95ffd487d539dab77b1bcee487117cd012324dca
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-12 13:23:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function calculatePayroll(employeeRecords) {
+	return employeeRecords.reduce(
+		(total, record) => total + allWagesFor.call(record),
+		0
+	);
 }
 
+function createEmployeeRecord(array) {
+	return {
+		firstName: array[0],
+		familyName: array[1],
+		title: array[2],
+		payPerHour: array[3],
+		timeInEvents: [],
+		timeOutEvents: [],
+	};
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+	return arrayOfArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+	this.timeInEvents.push({
+		type: 'TimeIn',
+		hour: parseInt(dateStamp.split(' ')[1]),
+		date: dateStamp.split(' ')[0],
+	});
+	return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+	this.timeOutEvents.push({
+		type: 'TimeOut',
+		hour: parseInt(dateStamp.split(' ')[1]),
+		date: dateStamp.split(' ')[0],
+	});
+	return this;
+}
+
+function hoursWorkedOnDate(date) {
+	let inEvent = this.timeInEvents.find((e) => e.date === date);
+	let outEvent = this.timeOutEvents.find((e) => e.date === date);
+	return (outEvent.hour - inEvent.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+	let hoursWorked = hoursWorkedOnDate.call(this, date);
+	return hoursWorked * this.payPerHour;
+}
+
+function allWagesFor() {
+	let allDates = this.timeInEvents.map((e) => e.date);
+	return allDates.reduce(
+		(total, date) => total + wagesEarnedOnDate.call(this, date),
+		0
+	);
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+	return srcArray.find((rec) => rec.firstName === firstName);
+}
